9f48aa8f1394c705cc13afb131377af233e56769
infinityInfinite <infinityInfinite217@gmail.com>
2022-03-21 19:45:49 +0530

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WIZ-CLI
 control your philips wiz smart light in your pc without wiz mobile app !!!
+
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import socket
 import sys
 import click
-from contextlib import closing
 import time
 import json
 import pyfiglet
+from contextlib import closing
+from scenes import get_scenes
+from termcolor import colored,cprint
 
 class Wiz:
     def __init__(self,ip,port):
@@ -39,9 +41,9 @@ class Wiz:
         return True
     
     def ask_color(self):
-        red = click.prompt("Please enter value of red",type=int,default=0,show_default=True)
-        green = click.prompt("Please enter value of green",type=int,default=0,show_default=True)
-        blue = click.prompt("Please enter value of blue",type=int,default=255,show_default=True)
+        red = click.prompt(colored("Please enter value of red","red"),type=int,default=0,show_default=True)
+        green = click.prompt(colored("Please enter value of green","green"),type=int,default=0,show_default=True)
+        blue = click.prompt(colored("Please enter value of blue","blue"),type=int,default=255,show_default=True)
         if red <= 255 and green <= 255 and blue <= 255:
             new_color = {"r":red,"g":green,"b":blue}
         else:
@@ -50,17 +52,40 @@ class Wiz:
             new_color = {"r":0,"g":0,"b":255}
             
         return new_color
+
+
+    def set_scene(self):
+        scenes = get_scenes()
+        for i,v in scenes.items():
+            print(colored(f"{i} : {v}","magenta"))
+        user_prompt = click.prompt(colored("choose an scene:","yellow"),type=int,default=1,show_default=True)
+        message = '{"id":1,"method":"setPilot","params":{"sceneId":0}}'
+        json_message = json.loads(message)
+        json_message['params']['sceneId'] = user_prompt
+        udp_client(self.ip,self.port,json.dumps(json_message))
         
-def udp_client(ip,port,message):
+    
+def udp_client(ip,port,message,return_response=False):
     s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,0)
-    print("Do Ctrl+C to exit the program !!")
+    print(colored("Do Ctrl+C to exit the program !!","blue"))
 
     while True:
-        s.sendto(message.encode('utf-8'),(ip,port))
-        print(f"\n [-] Data sent to client : {message},\n")
-        data,address = s.recvfrom(4096)
-        print(f"\n [-] Data recieved from Client : {data.decode('utf-8')},\n")
-        s.close()
+        try:
+            s.sendto(message.encode('utf-8'),(ip,port))
+            print(colored(f"\n [-] Data sent to client : {message},\n","green"))
+            s.settimeout(5)
+            data,address = s.recvfrom(4096)
+            
+            if return_response:
+                s.close()
+                return data
+            else:
+                print(colored(f"\n [-] Data recieved from Client : {data.decode('utf-8')},\n","green"))
+                s.close()
+        except socket.timeout:
+            s.close()
+            print(colored("[-] Looks like given ip is not wiz-light , try looking ip address in your wiz mobile app !","red"))
+            exit() 
         break
 
 
@@ -69,13 +94,12 @@ def error_check(port,ipaddr):
     port_open = False
     valid_ip = False
     overflow_error = False
-
+    wiz_light = False
     try:
         socket.inet_aton(ipaddr)
         valid_ip = True
     except socket.error:
         valid_ip = False
-
 
 
     with closing(socket.socket(socket.AF_INET,socket.SOCK_DGRAM)) as sock:
@@ -87,12 +111,12 @@ def error_check(port,ipaddr):
 
         
     if overflow_error == True:
-        print("[-] port must be below 65535")
+        print(colored("[-] port must be below 65535","red"))
     if valid_ip == False:
-        print('[-] ip address is not valid / checking port is available didnt take place ..')
+        print(colored('[-] ip address is not valid / checking port is available didnt take place ..',"red"))
     if valid_ip == True and port == False:
-        print('[-] given port is not open')
-
+        print(colored('[-] given port is not open',"red"))        
+        
     if overflow_error == False and valid_ip == True and port_open == True:
         return True
     else:
@@ -100,13 +124,11 @@ def error_check(port,ipaddr):
 
 
 def gettui():
-    print('welcome to pyauto cli..')
-
-
+    print(colored("Coming soon...","blue"))
 
 def welcome():
     header = pyfiglet.figlet_format("wiz-cli")
-    print(header)
+    print(colored(header,"red",attrs=["bold"]))
     
 @click.command()
 @click.option("-p","--port",default=38899,type=int)
@@ -117,7 +139,8 @@ def welcome():
 @click.option('-off','--turnoff',is_flag=True)
 @click.option('-on','--turnon',is_flag=True)
 @click.option('-dim','--dimming',type=int)
-def main(port,ipaddr,message,tui,changecolor,turnoff,turnon,dimming):
+@click.option('-ss','--setscene',is_flag=True)
+def main(port,ipaddr,message,tui,changecolor,turnoff,turnon,dimming,setscene):
     welcome()
     if(error_check(port,ipaddr)):
        wiz_light = Wiz(ipaddr,port)
@@ -129,9 +152,11 @@ def main(port,ipaddr,message,tui,changecolor,turnoff,turnon,dimming):
            wiz_light.turn_off()
        if dimming != None:
            wiz_light.change_dimming(dimming)
+       if setscene:
+           wiz_light.set_scene()
        if tui:
            print("tui comming soon")
     else:
-       print("some error")
+       print(colored("some error","red"))
     
 main()
